716d55cd68bf74329ac154791725e8a76dbee5c3
cchhriiss <spam.chris.mar.schultze@gmail.com>
2025-08-08 10:41:00 +0000

    pages

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
-export default {
-  base: './'
-};
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        workshops: 'workshops/index.html',
+        shows: 'shows/index.html',
+        portfolio: 'portfolio/index.html',
+        customs: 'customs/index.html',
+      }
+    }
+  }
+});
